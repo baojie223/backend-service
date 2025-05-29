e41acfd554da188be9ceb5315199d34f77e8a49b
Jie Bao <baoj@microsoft.com>
2025-05-29 10:51:43 +0800

    feat: Init

---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: true,
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        process.exit(1);
+    }
+}
+
+module.exports = { sequelize, testConnection };
